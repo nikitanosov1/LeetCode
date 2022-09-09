fef62d7a3874e2bc52e2a2700f280183377f936c
nikitanosov1 <nikinosov2002@mail.ru>
2022-09-09 13:25:10 +0300

    problem_1996 solved (Medium, The Number of Weak Characters in the Game, Sort pairs, Comparator, Nice idea)

--- src/main/java/problem_1996/Solution.java
+++ src/main/java/problem_1996/Solution.java
@@ -7,6 +7,7 @@ class Solution {
         Arrays.sort(properties,(a,b)-> (a[0]==b[0])?a[1]-b[1]:b[0]-a[0]);
         int max = -1,res = 0;
         for(int[] property: properties){
+            System.out.println(property[0] + " " + property[1]);
             if(property[1] < max) res++;
             else max = property[1];
         }
