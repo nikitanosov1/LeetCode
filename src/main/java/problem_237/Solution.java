526d2e727a3678a9191e2645c571fa1f80323636
nikitanosov1 <nikinosov2002@mail.ru>
2022-10-13 11:50:04 +0400

    problem_237 solved (Medium, Delete Node in a Linked List, funny task)

---
+++ src/main/java/problem_237/Solution.java
@@ -0,0 +1,48 @@
+package problem_237;
+
+import java.util.Objects;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    public static class ListNode {
+        private int val;
+        private ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            ListNode listNode = (ListNode) o;
+            return val == listNode.val && Objects.equals(next, listNode.next);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(val, next);
+        }
+    }
+}
