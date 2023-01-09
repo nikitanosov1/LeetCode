60f577e520754d921f208eea6014bdd9c6d5e898
nikitanosov1 <nikinosov2002@mail.ru>
2023-01-09 14:32:41 +0400

    problem_144 solved (Easy, Binary Tree Preorder Traversal, recursion)

---
+++ src/main/java/problem_144/Solution.java
@@ -0,0 +1,47 @@
+package problem_144;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+class Solution {
+    private List<Integer> result = new ArrayList<>();
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        recursion(root);
+        return result;
+    }
+
+    private void recursion(TreeNode root) {
+        if (root == null) return;
+        result.add(root.val);
+        recursion(root.left);
+        recursion(root.right);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            TreeNode treeNode = (TreeNode) o;
+            return val == treeNode.val && Objects.equals(left, treeNode.left) && Objects.equals(right, treeNode.right);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(val, left, right);
+        }
+    }
+}
