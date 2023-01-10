fc8a8cadfb39e0ca833ce965bb5f25d40c5ef31c
nikitanosov1 <nikinosov2002@mail.ru>
2023-01-10 13:01:30 +0400

    problem_100 solved (Easy, Same Tree)

---
+++ src/main/java/problem_100/Solution.java
@@ -0,0 +1,39 @@
+package problem_100;
+
+import java.util.Objects;
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p != null && q == null) return false;
+        if (p == null && q != null) return false;
+        // p and q are not null
+        return ((p.val == q.val) && isSameTree(q.left, p.left) && isSameTree(q.right, p.right));
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            TreeNode treeNode = (TreeNode) o;
+            return val == treeNode.val && Objects.equals(left, treeNode.left) && Objects.equals(right, treeNode.right);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(val, left, right);
+        }
+    }
+}
