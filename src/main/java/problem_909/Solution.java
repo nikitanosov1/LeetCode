4f5ebce3fadd337637bed330524e8c2bb78fabb3
nikitanosov1 <nikinosov2002@mail.ru>
2023-01-25 01:55:33 +0400

    problem_909 solved (Medium, Snakes and Ladders, BFS, Map)

--- src/main/java/problem_909/Solution.java
+++ src/main/java/problem_909/Solution.java
@@ -11,19 +11,22 @@ class Solution {
         for (int i = 1; i <= n * n; i++) {
             int row = n - 1 - (i - 1) / n;
             int column = ((n - 1 - row) % 2 == 0) ? (i - 1) - (n - 1 - row) * n : n - 1 - ((i - 1) - (n - 1 - row) * n);
-            //System.out.println(i + " " + row + " " + column);
 
             map.put(i, new ArrayList<>());
+
+
             for (int j = 1; j < 7; ++j) {
                 if (i + j > n * n) break;
-                map.get(i).add(i + j);
-            }
-
-            if (board[row][column] != -1) {
-                map.get(i).add(board[row][column]);
+                int nextRow = n - 1 - (i + j - 1) / n;
+                int nextColumn = ((n - 1 - nextRow) % 2 == 0) ? (i + j - 1) - (n - 1 - nextRow) * n : n - 1 - ((i + j - 1) - (n - 1 - nextRow) * n);
+                //System.out.println(i + j + " " + nextRow + " " + nextColumn);
+                if (board[nextRow][nextColumn] == -1) {
+                    map.get(i).add(i + j);
+                } else {
+                    map.get(i).add(board[nextRow][nextColumn]);
+                }
             }
         }
-
         System.out.println(map);
         // bfs
         Queue<Integer> queue = new LinkedList<>();
@@ -32,17 +35,17 @@ class Solution {
         queue.add(1);
         queueDeep.add(0);
         int result = -1;
+        isDone[1] = true;
         while (!queue.isEmpty()) {
-            System.out.println(queue);
             int x = queue.poll();
             int deep = queueDeep.poll();
-            System.out.println("x = " + x);
-            isDone[x] = true;
+            System.out.println("x = " + x + " deep = " + deep);
             if (x == n * n) {
-                result = Math.min(result, deep);
+                result = (result == -1) ? deep : Math.min(result, deep);
             }
             for (Integer next : map.get(x)) {
                 if (!isDone[next]) {
+                    isDone[next] = true;
                     queue.add(next);
                     queueDeep.add(deep + 1);
                 }
--- src/test/java/problem_909/SolutionTest.java
+++ src/test/java/problem_909/SolutionTest.java
@@ -18,4 +18,45 @@ class SolutionTest {
         int expected = 4;
         assertEquals(expected, new Solution().snakesAndLadders(board));
     }
+
+    @Test
+    void snakesAndLadders_2() {
+        int[][] board = new int[][]{
+                {-1,-1},
+                {-1,3}};
+        int expected = 1;
+        assertEquals(expected, new Solution().snakesAndLadders(board));
+    }
+
+    @Test
+    void snakesAndLadders_3() {
+        int[][] board = new int[][]{
+                {-1,4,-1},
+                {6,2,6},
+                {-1,3,-1}};
+        int expected = 2;
+        assertEquals(expected, new Solution().snakesAndLadders(board));
+    }
+
+    @Test
+    void snakesAndLadders_4() {
+        int[][] board = new int[][]{
+                {-1,1,2,-1},
+                {2,13,15,-1},
+                {-1,10,-1,-1},
+                {-1,6,2,8}};
+        int expected = 2;
+        assertEquals(expected, new Solution().snakesAndLadders(board));
+    }
+
+    @Test
+    void snakesAndLadders_5() {
+        int[][] board = new int[][]{
+                {-1,9,8},
+                {5,6,7},
+                {-1,3,4}};
+        int expected = 2;
+        assertEquals(expected, new Solution().snakesAndLadders(board));
+    }
+
 }
