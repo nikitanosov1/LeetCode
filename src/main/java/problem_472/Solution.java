58147119630eebb19319f48e6da939c0d1cf91de
nikitanosov1 <nikinosov2002@mail.ru>
2023-01-27 20:45:42 +0400

    problem_472 solved (Hard, Concatenated Words, HashSet, substring)

--- src/main/java/problem_472/Solution.java
+++ src/main/java/problem_472/Solution.java
@@ -17,7 +17,7 @@ class Solution {
         List<String> result = new ArrayList<>();
         // algo
         for (String s : words) {
-            int count = getCountOfWords(s);
+            int count = getCountOfWords(s, 0);
             if (count > 1) {
                 result.add(s);
             }
@@ -25,20 +25,20 @@ class Solution {
         return result;
     }
 
-    public int getCountOfWords(String s) {
+    public int getCountOfWords(String s, int startIndex) {
         int maxCountOfWords = -1;
 
-        for (int i = 0; i < s.length() - 1; ++i) {
-            if (set.contains(s.substring(0, i + 1))) {
-                int suffixCount = getCountOfWords(s.substring(i + 1));
+        for (int i = startIndex; i < s.length() - 1; ++i) {
+            if (set.contains(s.substring(startIndex, i + 1))) {
+                int suffixCount = getCountOfWords(s, i + 1);
                 if (suffixCount == -1) {
                     continue;
                 }
-                maxCountOfWords = Math.max(maxCountOfWords, 1 + getCountOfWords(s.substring(i + 1)));
+                maxCountOfWords = Math.max(maxCountOfWords, 1 + suffixCount);
             }
         }
         if (maxCountOfWords != -1) return maxCountOfWords;
-        if (set.contains(s)) {
+        if (set.contains(s.substring(startIndex))) {
             return 1;
         }
         return -1;
