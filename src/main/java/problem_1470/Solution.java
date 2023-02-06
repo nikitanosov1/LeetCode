c1ffcf27de3f67b85674296ec7c18d3ea041ab3e
nikitanosov1 <nikinosov2002@mail.ru>
2023-02-06 17:20:14 +0400

    problem_1470 solved (Easy, Shuffle the Array, array, bit operation)

--- src/main/java/problem_1470/Solution.java
+++ src/main/java/problem_1470/Solution.java
@@ -2,11 +2,31 @@ package problem_1470;
 
 class Solution {
     public int[] shuffle(int[] nums, int n) {
+        /*
+        Time: O(n)
+        Space: O(1)
+         */
+        for (int i = n; i < 2 * n; ++i) {
+            nums[i - n] |= nums[i] << 10;
+        }
+
+        int helpInt = (int) Math.pow(2, 10) - 1;
+        for (int i = n - 1; i >= 0; --i) {
+            nums[2 * i + 1] = nums[i] >> 10;
+            nums[2 * i] = nums[i] & helpInt;
+        }
+        return nums;
+
+        /*
+        Time: O(n)
+        Space: O(1), but we are using additional space for answer
+
         int[] shuffleArr = new int[2 * n];
         for (int i = 0; i < n; ++i) {
             shuffleArr[2 * i] = nums[i];
             shuffleArr[2 * i + 1] = nums[n + i];
         }
         return shuffleArr;
+        */
     }
 }
