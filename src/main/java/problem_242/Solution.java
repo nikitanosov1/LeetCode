af0044cc9170fcb08265fca64bd45ecb24a9c379
Nikita Nosov <nikinosov2002@mail.ru>
2026-03-01 22:38:24 +0400

    problem_242 solved (Easy, Valid Anagram, Map)

---
+++ src/main/java/problem_242/Solution.java
@@ -0,0 +1,28 @@
+package problem_242;
+/*
+ * @author Nikita Nosov
+ * Date: 3/1/2026
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (char c : t.toCharArray()) {
+            if (!map.containsKey(c)) {
+                return false;
+            }
+            if (map.get(c) == 1) {
+                map.remove(c);
+            } else {
+                map.put(c, map.get(c) - 1);
+            }
+        }
+        return map.isEmpty();
+    }
+}
